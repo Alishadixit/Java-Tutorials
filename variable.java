e812e9ed7d708d2890aea09bc6403f65f221eefa
Alisha Dixit <91393612+Alishadixit@users.noreply.github.com>
2022-07-10 00:34:43 +0530

    String functions
    String functions

---
+++ variable.java
@@ -0,0 +1,26 @@
+import javax.swing.text.AttributeSet;
+
+public class variable {
+        public static void main(String[] args) {
+            java.lang.String name ="Delisha";
+            String name2= "Dixit";
+            String name3=" and ";
+            String name4=name.replace("De","A");
+            System.out.println(name4);
+            System.out.println(name.substring(0,6));
+            System.out.println(name.charAt(0));
+            System.out.println(name + name3 +name2);
+            // substring is use to find text in between
+            // non primitive
+            // char at gives string of index zero
+            //name .length use for total length of string
+            // cocatenation is use to join two strings by +.
+            // name.replace use to replace text.
+           
+
+
+
+        }
+
+    }
+
