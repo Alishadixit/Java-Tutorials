c029871330035fa13002c38e6783452a9fa330d5
Alisha Dixit <91393612+Alishadixit@users.noreply.github.com>
2022-07-09 19:16:32 +0530

    Addition of two numbers
    Addition of two numbers

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+    public static void main(String[] args) {
+        System.out.println("Enter your digits");
+        int num1 =10;
+        int num2=3;
+        int num3=34;
+        int sum=num1+num2+num3;
+        System.out.println("The sum of three numbers is :" +sum);
+    }
+}
