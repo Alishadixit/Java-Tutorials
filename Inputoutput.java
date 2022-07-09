fe9c0f2e2347e564ddbfee29ee5c87b835145c5a
Alisha Dixit <91393612+Alishadixit@users.noreply.github.com>
2022-07-09 19:14:46 +0530

    Input output program
    Input output program

---
+++ Inputoutput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Inputoutput {
+ public static void main(String[] args) {
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter your name");
+  java.lang.String name=sc.nextLine();
+  System.out.println("my name is "+name);
+ }
+}
