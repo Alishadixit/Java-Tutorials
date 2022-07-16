412b26a2072ecf77c790731e77d2c7c4a1d50b76
Alisha Dixit <91393612+Alishadixit@users.noreply.github.com>
2022-07-16 18:50:44 +0530

    1D and 2D array

---
+++ array1.java
@@ -0,0 +1,9 @@
+public class array1 {
+    public static void main(String[] args) {
+        int[] marks={34,56,78};
+        System.out.println(marks[0]);
+//        multidimensional array are use for 2 arrays
+        int[][] finalmarks={{23,23,24},{54,34,12}};
+        System.out.println(finalmarks[0][2]);
+    }
+}
