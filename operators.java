dfbc7426f7543319d74bdb5cc9240fe5fbe7dccf
Alisha Dixit <91393612+Alishadixit@users.noreply.github.com>
2022-07-16 18:53:27 +0530

    Operators and its functions

---
+++ operators.java
@@ -0,0 +1,29 @@
+public class operators {
+    public static void main(String[] args){
+        int num1= 8;
+        int num2= 4;
+        int sum = num1+num2;
+        System.out.println("the sum of two digits is : " + sum);
+        int num3= 8;
+        int num4= 4;
+        int multi= num1*num2;
+        System.out.println("the product of two no is : " +multi);
+        int num5= 6;
+        int num6= 4;
+        int divide= num5/num6;
+        System.out.println("the divide of two no is : " +divide);
+//  Unary Operators
+        int numa=1;
+        System.out.println(++numa);
+        System.out.println(numa);
+        System.out.println(numa++);
+        //Decrement Operators
+//        in this value doesnot chnge in numb--
+        int numb =8;
+        System.out.println(--numb);
+       System.out.println(numb);
+        System.out.println(numb--);
+
+
+    }
+}
