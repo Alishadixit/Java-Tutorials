91eae11cf0cb1f5338752c114adb81784a23ce0f
Alisha Dixit <91393612+Alishadixit@users.noreply.github.com>
2022-07-16 18:52:47 +0530

    Casting functions

---
+++ casting.java
@@ -0,0 +1,13 @@
+public class casting {
+    public static void main(String[] args) {
+        int num1=87;
+        float num2=17.8f;
+        float total = (float)num1+ num2;
+        System.out.println(total);
+//        using final keyword
+       final double PIE=3.14;
+        System.out.println(PIE);
+
+    }
+
+}
