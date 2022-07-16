d993523dbdf90a80bfe56e5d729234644deda66b
Alisha Dixit <91393612+Alishadixit@users.noreply.github.com>
2022-07-16 18:54:00 +0530

    Using math class

---
+++ max.java
@@ -0,0 +1,9 @@
+public class max {
+    public static void main(String[] args) {
+        int a= 45;
+        int b=32;
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.random());
+    }
+}
