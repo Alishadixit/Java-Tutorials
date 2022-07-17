4456a112c4ef000343ffac93b37e78b7684c75d4
Alisha Dixit <91393612+Alishadixit@users.noreply.github.com>
2022-07-17 23:16:12 +0530

    WAP to print natural number up to 100 in reverse and vice versa using for loop.

---
+++ pprogram2.java
@@ -0,0 +1,15 @@
+public class pprogram2 {
+    public static void main(String[] args) {
+        for(int i = 100; i>=1; i--){
+            System.out.println(i);
+        }
+        for (int j=1;j<=100;j++){
+            System.out.println(j);
+        }
+        int a=0;
+        for( int n; a<=10;a++){
+            System.out.println("Hello alisha");
+
+        }
+    }
+}
