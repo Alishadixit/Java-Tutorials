5edf119034e383eb868a95a1a712ccf3c0c5adbc
Alisha Dixit <91393612+Alishadixit@users.noreply.github.com>
2022-07-17 23:21:57 +0530

    WAP to print sum of array using for loop.

---
+++ arrayprog.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class arrayprog {
+    public static void main(String[] args) {
+        int[] arr={2,3,4,5,6};
+         int sum=0;
+         for (int arrs:arr){
+//             System.out.println(arrs);
+             sum=sum+arrs;
+//             System.out.println(sum);
+
+         }
+        System.out.println(sum);
+    }
+}
