9362c3ed296e36212de425678bd9561c6220da28
Alisha Dixit <91393612+Alishadixit@users.noreply.github.com>
2022-07-17 23:17:27 +0530

    WAP to print sum of no up to 1000 using for loop.

---
+++ program3.java
@@ -0,0 +1,10 @@
+public class program3 {
+    public static void main(String[] args) {
+        int num=1000;
+        int sum = 0;
+        for (int i=num; i>=1;--i){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
