f10d71521dca89d14202637504f44b0a72097603
Alisha Dixit <91393612+Alishadixit@users.noreply.github.com>
2022-07-17 23:13:45 +0530

    WAP to enter natural no given by user using do while loop.

---
+++ program1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class program1 {
+    public static void main(String[] args) {
+        int num;
+
+        do {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter your num");
+            num=sc.nextInt();
+            System.out.println("here is your num");
+
+        }while (num>=0);
+        System.out.println("The End");
+
+        }
+    }
+
+
