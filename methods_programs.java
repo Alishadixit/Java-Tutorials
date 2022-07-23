5d5be07243a121f7a0112c9e452803cf8b83d235
Alisha Dixit <91393612+Alishadixit@users.noreply.github.com>
2022-07-23 18:12:10 +0530

    WAP to print table of any given number.

---
+++ methods_programs.java
@@ -0,0 +1,12 @@
+public class methods_programs {
+    static void multiplication(int n){
+        for (int i=1;i<=10;i++) {
+            System.out.format("%d * %d= %d \n",n,i,n*i);
+        }
+
+    }
+    public static void main(String[] args) {
+        multiplication(9);
+
+    }
+}
